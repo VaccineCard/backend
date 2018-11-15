976806f539faa31d51a2d46d759d42f2a6fc80cc
Acidiney Dias <acidineydias@gmail.com>
2018-11-15 16:58:02 +0100

    Adicionando RefreshToken

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'jwt.auth' => Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+        'jwt.refresh' => Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 
     /**
